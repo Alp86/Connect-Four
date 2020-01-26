2523191749862f4c73bee30bda69afa6bdf3ca8d
Alp86 <alperen.kan.86@gmail.com>
2020-01-26 14:56:41 +0100

    added blinking animation to winning four slots

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 (function() {
     var currentplayer = "player1";
-
     var $columns = $(".column");
-
     var colIndex;
-
     var rowIndex;
-
     var colSelector;
-
     var $cursor = $("#cursor");
+    var $winner = $("#winner");
+    var $winningFour = [];
+
     $cursor.addClass(currentplayer);
 
+    $columns.on("click", addSlot);
 
-    $columns.on("click", function(e) {
+    function addSlot(e) {
 
         var $col = $(e.currentTarget);
 
@@ -48,24 +47,48 @@
         var $diagonalMinus = diagonal[1];
 
         // check for victory
-        if (checkForVictory($slotsInCol, "col")) {
-            console.log(currentplayer + " won!");
-            location.reload();
-        } else if (checkForVictory($slotsInRow, "row")) {
-            console.log(currentplayer + " won!");
-            location.reload();
-        } else if (checkForVictory($diagonalPlus, "diaPlus")) {
-            console.log(currentplayer + " won!");
-            location.reload();
-        } else if (checkForVictory($diagonalMinus, "diaMinus")) {
-            console.log(currentplayer + " won!");
-            location.reload();
-        } else {
-            switchPlayer();
-        }
-        console.log("---------------------------");
+        checkForWinner($slotsInCol, $slotsInRow, $diagonalPlus, $diagonalMinus);
 
-    });
+    }
+
+    // $columns.on("click", function click(e) {
+    //
+    //     var $col = $(e.currentTarget);
+    //
+    //     var $slotsInCol = $col.children();
+    //
+    //     for (var i = $slotsInCol.length-1; i >= 0; i--) {
+    //         if (
+    //             !$slotsInCol.eq(i).hasClass("player1") &&
+    //             !$slotsInCol.eq(i).hasClass("player2"))
+    //         {
+    //             $slotsInCol.eq(i).addClass(currentplayer);
+    //             break;
+    //         }
+    //     }
+    //
+    //     // console.log("i: ", i);
+    //     if (i === -1) {
+    //         i = 0;
+    //         return;
+    //     }
+    //     rowIndex = i;
+    //     colIndex = $(e.currentTarget).index();
+    //
+    //     console.log("i is: ", i);
+    //
+    //     // create an array out of the slots in the row in which the last chip was placed
+    //     // i = row index;
+    //     var rowElements = ".row" + i;
+    //     var $slotsInRow = $(rowElements);
+    //     var diagonal = createDiagonal();
+    //     var $diagonalPlus = diagonal[0];
+    //     var $diagonalMinus = diagonal[1];
+    //
+    //     // check for victory
+    //     checkForWinner($slotsInCol, $slotsInRow, $diagonalPlus, $diagonalMinus);
+    //
+    // });
 
     // console.log("diagonalPlus: ", $diagonalPlus);
     // console.log($diagonalMinus);
@@ -100,6 +123,30 @@
         return [$diagonalPlus, $diagonalMinus];
     }
 
+    function checkForWinner(col, row, diaPlus, diaNeg) {
+
+        var message;
+        var method;
+
+        if (checkForVictory(col) || checkForVictory(row) || checkForVictory(diaPlus) || checkForVictory(diaNeg)) {
+
+            message = currentplayer + " wins!\n\nRestart game?";
+
+            for (var i = 0; i < $winningFour.length; i++) {
+                $winningFour[i].addClass("won")
+            }
+
+            $columns.off("click", addSlot);
+
+            // setTimeout(function(){
+            //     alert(message);
+            //     location.reload();
+            // }, 1500);
+
+        } else {
+            switchPlayer();
+        }
+    }
 
     function checkForVictory($slots, rc) {
         // we will do some logic to check for victory
@@ -110,11 +157,13 @@
 
             if ($slots.eq(i).hasClass(currentplayer)) {
                 count++;
+                $winningFour.push($slots.eq(i));
                 if (count == 4) {
                     return true;
                 }
             } else {
                 count = 0;
+                $winningFour = [];
             }
         }
 
@@ -130,9 +179,11 @@
 
             $cursor.addClass(currentplayer);
 
-            $(colSelector).css({
-                borderTopColor: "yellow"
-            });
+            $(colSelector).addClass("p2");
+
+            // $(colSelector).css({
+            //     borderTopColor: "yellow"
+            // });
 
         } else {
             $cursor.removeClass(currentplayer);
@@ -141,9 +192,11 @@
 
             $cursor.addClass(currentplayer);
 
-            $(colSelector).css({
-                borderTopColor: "red"
-            });
+            $(colSelector).addClass("p2");
+
+            // $(colSelector).css({
+            //     borderTopColor: "red"
+            // });
         }
     }
 
@@ -154,15 +207,16 @@
         // $(".active").removeClass("active");
         colSelector = ".col" + $(e.currentTarget).index();
 
-        $(colSelector).css({
-            borderTopColor: (currentplayer=="player1") ? "red" : "yellow"
-        });
+        // $(colSelector).css({
+        //     borderTopColor: (currentplayer=="player1") ? "red" : "yellow"
+        // });
 
-        $(colSelector).addClass("active");
+        $(colSelector).addClass( (currentplayer=="player1") ? "p1" : "p2" );
 
     }).on("mouseleave", function() {
 
-        $(".active").removeClass("active");
+        $(".p1").removeClass("p1");
+        $(".p2").removeClass("p2");
     });
 
 
@@ -181,9 +235,63 @@
 
         // $active = $(".active");
 
-
-
     });
+
+    // winner announcement animation
+    // var curr = 0;
+    // var frames = 45;
+    // var fontsize = 200;
+    // var rotation = 360;
+    //
+    // function winnerAnim() {
+    //
+    //
+    //     var winnerText = "<div>" + currentplayer.toUpperCase() + " WINS!</div>";
+    //
+    //     $winner.html(winnerText);
+    //
+    //     var $winnerDiv = $("#winner div");
+    //
+    //     if (curr <= frames) {
+    //
+    //         $winnerDiv.css({
+    //             fontSize: curr * fontsize/frames + "px",
+    //             transform: "rotate(" + curr * rotation/frames + "deg)"
+    //         });
+    //         curr++;
+    //         requestAnimationFrame(winnerAnim);
+    //     }
+    // }
 
 
 })();
+
+
+
+
+
+/*
+The board has six rows and seven columns  -- done
+
+Two players take turns selecting a column to drop their checker into -- done
+
+When a player wins, a message appears to announce the victory -- have to implement message box
+
+After a player wins, it should be possible to reset the game and play again -- currently automatically resets
+
+The gameplay should involve at least one animation (for example, the checkers could fall into their slot rather than just appear instantaneously)
+-- still to do
+
+Bonus features
+Here are several ideas to make the game more interesting if you have the time
+
+After a player wins, visually indicate which four pieces on the board satisfied the victory condition
+
+Allow players to drag their pieces across the screen and drop them into their desired column using their mouse (or finger on touch screens)
+
+Allow players to play using only their keyboard
+
+Allow players at the beginning of the game to increase the number of columns on the board and the number of connected pieces that are required to win
+
+Allow a single player to play against the computer
+*/
